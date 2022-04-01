d3000dfd1f2c5561e91e60534b0d41bc3cd88084
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-04-01 11:08:55 +0500

    answers

--- List08.py
+++ List08.py
@@ -1,3 +1,6 @@
+import re
+
+
 def main(fruits):
     """
     A list called "fruits" is given and is five in length and contains at least one "apple". Remove the apples from the list and return the list.
@@ -6,17 +9,9 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    if fruits[0] == 'apple':
-        del fruits[0]
-    if fruits[1] == 'apple':
-        del fruits[1]
-    if fruits[2] == 'apple':
-        del fruits[2]
-    if fruits[3] == 'apple':
-        del fruits[3]
-    if fruits[4] == 'apple':
-        del fruits[4]
-
-
+    for i in fruits:
+        if i == 'apple':
+            fruits.remove(i)
     return fruits
 
+print(main(["apple", "banana", "apple", "pear", "apple"]))
